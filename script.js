50bd4e236d8c1930dd3dc351a70824a60cbc2994
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-19 20:34:15 +0500

    "Fixed typo in product object property from 'discription' to 'Description' and added new product 'Pent' to the list."

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const products = [
-    { name: "Shirt", price: 75, discription: "Shirtsss" , image: "Assets/p1.png" },
-    { name: "Jacket", price: 80, discription: "Jacketss" , image: "Assets/p2.png" },
-    { name: "Dress", price: 68, discription: "Dresss" , image: "Assets/p4.png" },
-    { name: "Frawk", price: 70, discription: "Capss" , image: "Assets/p7.png" },
-    { name: "Tracksuit", price: 100, discription: "Tracksuit" , image: "Assets/dom-hill-nimElTcTNyY-unsplash.jpg" },
-    { name: "T Shirt", price: 99, discription: "T Shirt" , image: "Assets/ian-dooley-wtFc4o4Bir8-unsplash.jpg" },
-    { name: "Cap", price: 68, discription: "Cap" , image: "Assets/zakaria-issaad-Pg7m1VqJ-b4-unsplash.jpg" },
-    { name: "Frawk", price: 70, discription: "Capss" , image: "Assets/p7.png" },
+    { name: "Shirt", price: 75, Description: "Shirtsss" , image: "Assets/p1.png" },
+    { name: "Jacket", price: 80, Description: "Jacketss" , image: "Assets/p2.png" },
+    { name: "Dress", price: 68, Description: "Dresss" , image: "Assets/p4.png" },
+    { name: "Frawk", price: 70, Description: "Capss" , image: "Assets/p7.png" },
+    { name: "Tracksuit", price: 100, Description: "Tracksuit" , image: "Assets/dom-hill-nimElTcTNyY-unsplash.jpg" },
+    { name: "T Shirt", price: 99, Description: "T Shirt" , image: "Assets/ian-dooley-wtFc4o4Bir8-unsplash.jpg" },
+    { name: "Cap", price: 68, Description: "Cap" , image: "Assets/zakaria-issaad-Pg7m1VqJ-b4-unsplash.jpg" },
+    { name: "Pent", price: 70, Description: "Pent" , image: "Assets/download.jpg" },
   ];
   
   const productGrid = document.getElementById("productGrid");
@@ -21,7 +21,7 @@ const products = [
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
         <p class="price">$${product.price}</p>
-        <p class="text-black">Discription : ${product.discription}</p>
+        <p class="text-black">Description : ${product.Description}</p>
         <button class="add-to-cart">Add To Cart</button>
         <button class="buy-now">Buy Now</button>
       `;
@@ -36,6 +36,6 @@ const products = [
 
   // function myFunction(){
   //  var a = search.value ;
-  //  if()
+  //  if(a = )
   // }
 
